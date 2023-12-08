49c01deb75275e40e496b994c3fb6d83c68e1302
guilherme <88243712+guilhermeezx@users.noreply.github.com>
2023-12-08 08:15:57 -0300

    Configuração do postgres

--- banco.js
+++ banco.js
@@ -2,12 +2,9 @@ const sequelize = require("sequelize");
 require('dotenv').config()
 
 const conexao = new sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+  process.env.DB_HOST,
     {
-        dialect:"mysql",
-        host:process.env.DB_HOST
+            dialect:"postgres"
     }
 )
 
